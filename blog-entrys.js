a4149c460676fa2ab8623fadbffdd615761d6dc0
Julif <julif-@live.com>
2020-07-17 16:30:46 -0300

    v 0.1.3

--- blog-entrys.js
+++ blog-entrys.js
@@ -10,14 +10,16 @@ var blog_entrys = [
     { 
       titulo: 'entrada de blog 2'  ,
       id: 'blog_2' ,
-      fecha: [2020, 7, 16]
+      fecha: [2020, 7, 16],
+      entrada:'<p> texto de entrada 2</p>'
    
     },
   
     { 
       titulo: 'entrada de blog 1',
-      id: 'blog_2', 
-      fecha: [2020, 7, 15]
+      id: 'blog_1', 
+      fecha: [2020, 7, 15],
+      entrada:'<p> texto de entrada 1</p>'
     
     }
 ]; 
