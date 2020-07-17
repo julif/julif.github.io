9d307e1f41b7768e8eebf3752bedee7de8b8e582
Julif <julif-@live.com>
2020-07-17 00:07:34 -0300

    v0.1.1

---
+++ devblog-entrys.js
@@ -0,0 +1,24 @@
+/* { 
+    titulo: ''  ,
+    id: ,
+    fecha:'',
+    entrada: ' '  
+}, */
+
+
+var devblog_entrys = [
+    { 
+      titulo: 'entrada de devblog 2'  ,
+      id: 'devblog_2' ,
+      fecha: [2020, 7, 16]
+   
+    },
+  
+    { 
+      titulo: 'entrada de devblog 1',
+      id: 'devblog_1', 
+      fecha: [2020, 7, 15]
+    
+    }
+]; 
+    
