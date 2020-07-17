522fefa6b4b14e0ac0d085e5cee5117658bbd655
Julif <julif-@live.com>
2020-07-16 23:43:43 -0300

    v0.1

---
+++ blog-entrys.js
@@ -0,0 +1,24 @@
+/* { 
+    titulo: ''  ,
+    id: ,
+    fecha:'',
+    entrada: ' '  
+}, */
+
+
+var blog_entrys = [
+    { 
+      titulo: 'entrada de blog 2'  ,
+      id: 'blog_2' ,
+      fecha: [2020, 7, 16]
+   
+    },
+  
+    { 
+      titulo: 'entrada de blog 1',
+      id: 'blog_2', 
+      fecha: [2020, 7, 15]
+    
+    }
+]; 
+    
