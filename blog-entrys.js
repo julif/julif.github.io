ddf52fa77c3e239a97861358bbdaadbca6be1a1a
Julif <julif-@live.com>
2020-10-07 18:33:51 -0300

    v 2 0.1

---
+++ blog-entrys.js
@@ -0,0 +1,36 @@
+/* { 
+    titulo: ''  ,
+    id: ,
+    fecha:'',
+    entrada: ' '  
+}, */
+
+
+var blog_entrys = [
+  { 
+    titulo: 'entrada de blog 4'  ,
+    id: 'blog_004' ,
+    fecha: [2020, 7, 17]
+ 
+  },
+  { 
+    titulo: 'entrada de blog 3'  ,
+    id: 'blog_003' ,
+    fecha: [2020, 7, 17]
+ 
+  },
+    { 
+      titulo: 'entrada de blog 2'  ,
+      id: 'blog_002' ,
+      fecha: [2020, 7, 16]
+   
+    },
+
+    { 
+      titulo: 'entrada de vimeo',
+      id: 'blog_001', 
+      fecha: [2020, 7, 15]
+    
+    }
+]; 
+    
