37397de6ad681d78d91fe7cb2066fafc9e53c68a
Julif <julif-@live.com>
2020-10-08 16:15:42 -0300

    v 2 0.1.1

--- blog-entrys.js
+++ blog-entrys.js
@@ -11,6 +11,7 @@ var blog_entrys = [
     titulo: 'entrada de blog 4'  ,
     id: 'blog_004' ,
    thumbnail: 'media/ko-fi.png' ,
+   snippet: 'snippet ' ,
     fecha: [2020, 7, 17]
  
   },
@@ -18,6 +19,7 @@ var blog_entrys = [
     titulo: 'entrada de blog 3'  ,
     id: 'blog_003' ,
     thumbnail: 'media/ko-fi.png' ,
+    snippet: 'snippet ' ,
     fecha: [2020, 7, 17]
  
   },
@@ -25,6 +27,7 @@ var blog_entrys = [
       titulo: 'entrada de blog 2'  ,
       id: 'blog_002' ,
       thumbnail: 'media/ko-fi.png' ,
+      snippet: 'snippet ' ,
       fecha: [2020, 7, 16]
    
     },
@@ -33,6 +36,7 @@ var blog_entrys = [
       titulo: 'entrada de vimeo',
       id: 'blog_001', 
       thumbnail: 'img/default.png' ,
+      snippet: 'snippet ' ,
       fecha: [2020, 7, 15]
     
     }
