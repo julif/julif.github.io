4f64a43a978376ef3fc6b3de55866c423bc6c5ac
Julif <julif-@live.com>
2020-10-08 15:09:06 -0300

    v 2 0.1.1
    thubmnail

--- devblog-entrys.js
+++ devblog-entrys.js
@@ -8,30 +8,30 @@
 
 var blog_entrys = [
   { 
-    titulo: 'entrada de blog 4'  ,
-    id: 'blog_004' ,
-   thumbnail: 'media/ko-fi.png' ,
+    titulo: 'entrada de devblog 4'  ,
+    id: 'devblog_004' ,
+    thumbnail: 'img/javascript.png' ,
     fecha: [2020, 7, 17]
  
   },
   { 
-    titulo: 'entrada de blog 3'  ,
-    id: 'blog_003' ,
+    titulo: 'entrada de devblog 3'  ,
+    id: 'devblog_003' ,
     thumbnail: 'media/ko-fi.png' ,
     fecha: [2020, 7, 17]
  
   },
     { 
-      titulo: 'entrada de blog 2'  ,
-      id: 'blog_002' ,
-      thumbnail: 'media/ko-fi.png' ,
+      titulo: 'entrada de devblog 2'  ,
+      id: 'devblog_002' ,
+      thumbnail: 'img/javascript.png' ,
       fecha: [2020, 7, 16]
    
     },
 
     { 
-      titulo: 'entrada de vimeo',
-      id: 'blog_001', 
+      titulo: 'entrada de devblog 1',
+      id: 'devblog_001', 
       thumbnail: 'img/default.png' ,
       fecha: [2020, 7, 15]
     
